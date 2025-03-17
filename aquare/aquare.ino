624be8147dbef16db4a6353339caf9a12720570e
João Vitor <thecryzin@gmail.com>
2025-03-17 17:41:38 -0300

    feat: bomb + CSMS sensor

--- aquare/aquare.ino
+++ aquare/aquare.ino
@@ -11,21 +11,25 @@
 //BH1750
 #include <BH1750.h>
 
+#define SERIAL_DEBUG   true
+
 #define TFT_CS        10
 #define TFT_RST        9
 #define TFT_DC         8
-
-#define SERIAL_DEBUG   true    
 
 #define BMP_ERROR_LED 7
 #define BH1750_ERROR_LED 6
 
 #define LAMP_PIN 5
+#define BOMB_PIN 4
+#define CSMS_PIN A3
 
-int BMP_READY = true;
-int BH1750_READY = true;
+bool BMP_READY = true;
+bool BH1750_READY = true;
+bool CSMS_READY = true;
 
 bool LAMP_STATUS = false;
+bool BOMB_STATUS = false;
 
 Adafruit_BMP280 bmp;
 
@@ -41,15 +45,18 @@ void setup(void) {
   pinMode(BH1750_ERROR_LED, OUTPUT);
 
   pinMode(LAMP_PIN, OUTPUT);
+  pinMode(BOMB_PIN, OUTPUT);
 
   if (!bmp.begin(0x76)) {
-    Serial.println("Não foi possivel localizar o sensor BMP 280");
     BMP_READY = false;
   }
 
   if (!lightMeter.begin()) {
-    Serial.println("Não foi possivel localizar o sensor BH 1750");
     BH1750_READY = false;
+  }
+
+  if (analogRead(CSMS_PIN) == 0) {
+    CSMS_READY = false;
   }
 
   checkSensorError();
@@ -64,6 +71,7 @@ void loop() {
 
   checkSensorError();
   checkLuminosity();
+  checkMoisture();
 
   displaySensorData();
   
@@ -74,14 +82,22 @@ void loop() {
   delay(5000);
 }
 
-void formatAndDraw(float value, const char* unit, uint16_t color, int textSize, int x, int y) {
-  char buffer[16];  
+void formatAndDraw(const char* label, float value, const char* unit, uint16_t color, int textSize, int x, int y) {
+  char buffer[30];  
+  char valueStr[10];  
+
   if (isnan(value)) {
-    snprintf(buffer, sizeof(buffer), "Err%s", unit);
+    strcpy(buffer, label);
+    strcat(buffer, ": Err");
   } else {
-    dtostrf(value, 6, 2, buffer);
-    strncat(buffer, unit, sizeof(buffer) - strlen(buffer) - 1);
+    dtostrf(value, 6, 2, valueStr);
+    strcpy(buffer, label);
+    strcat(buffer, ": ");
+    strcat(buffer, valueStr);
+    strcat(buffer, " ");
+    strcat(buffer, unit);
   }
+  
   drawText(buffer, color, textSize, x, y);
 }
 
@@ -90,24 +106,33 @@ void displaySensorData() {
     float temperature = bmp.readTemperature();
     float pressure = bmp.readPressure() / 1013.25F / 100.0F;
     float altitude = bmp.readAltitude(1013.25);
-    formatAndDraw(temperature, "C", ST77XX_WHITE, 2, 0, 0);
-    formatAndDraw(pressure, "atm", ST77XX_WHITE, 2, 0, 25);
-    formatAndDraw(altitude, "m", ST77XX_WHITE, 2, 0, 50);
+
+    formatAndDraw("Temperatura", temperature, "C", ST77XX_WHITE, 1, 0, 0);
+    formatAndDraw("Pressao", pressure, "atm", ST77XX_WHITE, 1, 0, 10);
+    formatAndDraw("Altitude", altitude, "m", ST77XX_WHITE, 1, 0, 20);
   }
 
   if (BH1750_READY) {
     uint16_t luminosity = lightMeter.readLightLevel();
-    char luxBuffer[12];
-    snprintf(luxBuffer, sizeof(luxBuffer), "%ulux", luminosity);
-    drawText(luxBuffer, ST77XX_WHITE, 2, 0, 75);
-
-    drawText("Lampada", LAMP_STATUS ? ST77XX_GREEN : ST77XX_RED, 2, 0, 100);
+    char luxBuffer[25];
+    snprintf(luxBuffer, sizeof(luxBuffer), "Luminosidade: %ulux", luminosity);
+    drawText(luxBuffer, ST77XX_WHITE, 1, 0, 30);
   }
+
+  if (CSMS_READY) {
+    int16_t moisture = analogRead(CSMS_PIN);
+    char moistureBuffer[20];
+    snprintf(moistureBuffer, sizeof(moistureBuffer), "Umidade (01): %u", moisture);
+    drawText(moistureBuffer, ST77XX_WHITE, 1, 0, 40);
+  }
+
+  drawText("Bomba", BOMB_STATUS ? ST77XX_GREEN : ST77XX_RED, 2, 0, 100);
+  drawText("Lampada", LAMP_STATUS ? ST77XX_GREEN : ST77XX_RED, 2, 0, 125);
 }
 
 void debug() {
+  Serial.println("DEBUG BMP280");
   if (BMP_READY) {
-    Serial.println("DEBUG BMP280");
     Serial.print(F("Temperatura: "));
     Serial.print(bmp.readTemperature());
     Serial.println(" C");
@@ -117,27 +142,41 @@ void debug() {
     Serial.print(F("Altitude Aprox: "));
     Serial.print(bmp.readAltitude(1013.25));
     Serial.println(" m");
-    Serial.println();
   } else {
     Serial.println("Erro: BMP280 não detectado");
   }
+  Serial.println();
 
+  Serial.println("DEBUG BH1750");
   if (BH1750_READY) {
-    Serial.println("DEBUG BH1750");
     uint16_t lux = lightMeter.readLightLevel();
     Serial.print("Luminosidade: ");
     Serial.print(lux);
     Serial.println(" lux");
     Serial.println();
 
-
     Serial.println("DEBUG LAMP STATUS");
     Serial.print("Status da lampada: ");
     Serial.println(LAMP_STATUS ? "Ligado" : "Desligado");
-    Serial.println();
   } else {
     Serial.println("Erro: BH1750 não detectado");
   }
+  Serial.println();
+
+  Serial.println("DEBUG CSMS 01");
+  if (CSMS_READY) {
+    uint16_t lux = lightMeter.readLightLevel();
+    Serial.print("Nivel de umidade: ");
+    Serial.println(analogRead(CSMS_PIN));
+    Serial.println();
+
+    Serial.println("DEBUG BOMB STATUS");
+    Serial.print("Status da bomba: ");
+    Serial.println(BOMB_STATUS ? "Ligado" : "Desligado");
+  } else {
+    Serial.println("Erro: CSMS 01 não detectado");
+  }
+  Serial.println();
 }
 
 void drawText(char *text, uint16_t color, int size, int x, int y) {
@@ -165,6 +204,12 @@ void testSensors() {
     BH1750_READY = false;
   } else {
     BH1750_READY = true;
+  }
+
+  if (analogRead(CSMS_PIN) > 0) {
+    CSMS_READY = true;
+  } else {
+    CSMS_READY = false;
   }
 }
 
@@ -198,5 +243,20 @@ void checkLuminosity() {
   } else {
     LAMP_STATUS = false;
     digitalWrite(LAMP_PIN, LOW);
+  }
+}
+
+void checkMoisture() {
+  if (CSMS_READY) {
+    if (analogRead(CSMS_PIN) >= 400) {
+      BOMB_STATUS = true;
+      digitalWrite(BOMB_PIN, HIGH);
+    } else {
+      BOMB_STATUS = false;
+      digitalWrite(BOMB_PIN, LOW);
+    }
+  } else {
+    BOMB_STATUS = false;
+    digitalWrite(BOMB_PIN, LOW);
   }
 }
