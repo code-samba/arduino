03dd4ad86cf6e5f4f632d814afd928b63c35e85a
Vinicius Bertuzzo Lima <vinicius.bertuzzo@gmail.com>
2025-04-30 09:32:32 -0300

    fix: aquare small improvments

--- aquare/aquare.ino
+++ aquare/aquare.ino
@@ -1,79 +1,136 @@
-#include <NewPing.h>
-#include <Adafruit_GFX.h>    
-#include <Adafruit_ST7735.h> 
-#include <SPI.h>
+#include <Servo.h>
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
 
-#define TRIGGER_PIN 6
-#define ECHO_PIN 7
+#define servoPinP 11
+#define servoPinS 10
+#define servoPinT 8
+#define servoPinQ 9
 
-#define TFT_CS        10
-#define TFT_RST        9
-#define TFT_DC         8
+#define col 16
+#define lin  2
+#define ende  0x27
 
-#define RADIUS 5
-#define MAX_DISTANCE 150
-#define MAX_HEIGHT 100
-#define MAX_VOLUME 1000
+LiquidCrystal_I2C lcd(ende, col, lin);
 
-NewPing sonar(TRIGGER_PIN, ECHO_PIN, MAX_DISTANCE);
-Adafruit_ST7735 tft = Adafruit_ST7735(TFT_CS, TFT_DC, TFT_RST);
+Servo p, s, t, q;
+int pos;
+
+int port = 1;
+bool ready = false;
+
+//172800
+const unsigned long tempoDeEspera = 172800;
 
 void setup() {
-  Serial.begin(9600);
-  Serial.println("Initializing...");
+  pinMode(7, INPUT_PULLUP); 
+  
+  p.attach(servoPinP); p.write(0); delay(1000);
+  s.attach(servoPinS); s.write(0); delay(1000);
+  t.attach(servoPinT); t.write(0); delay(1000);
+  q.attach(servoPinQ); q.write(0); delay(1000);
 
-  tft.initR(INITR_BLACKTAB);
-  Serial.println("Initialized");
+  lcd.init();
+  lcd.backlight();
+  lcd.clear();
 }
 
-void loop() {
-  tft.fillScreen(ST77XX_BLACK);
-  long distance = sonar.ping_cm();
-  
-  if (distance > 0) {
-    float volume = calculateVolume(distance);
-    float waterLevel = calculateWaterLevel(volume);
-
-    drawText("Volume (L):", ST77XX_WHITE, 1, 10, 10);
-    drawFixedBox();
-    if (volume > 0) {
-      drawWaterLevel(waterLevel);
-    }
-
-    char volumeStr[10];
-    dtostrf(volume / 1000, 6, 2, volumeStr);
-    drawText(volumeStr, ST77XX_WHITE, 1, 70, 10);
-  } else {
-    drawText("Erro no sensor", ST77XX_RED, 1, 10, 10);
+void loop() { 
+  if (digitalRead(7) == LOW) {
+    ready = true;
   }
 
-  delay(1000);
+  if (ready) {
+    esperarComDisplay(tempoDeEspera);
+
+    if (port == 1) {
+      incrasePos(180, p);
+    }
+
+    if (port == 2) {
+      incrasePos(180, s);
+    }
+  
+    if (port == 3) {
+      incrasePos(180, t);
+    }
+  
+    if (port == 4) {
+      incrasePos(180, q);
+    }
+
+    if (port < 4) {
+      port += 1;
+    } else {
+      port = 1;
+      ready = false;
+
+      lcd.clear();
+      lcd.setCursor(0, 0);
+      lcd.print("Resetando...");
+
+      decrasePos(180, q, 0);
+      decrasePos(180, t, 0);
+      decrasePos(180, s, 0);
+      decrasePos(180, p, 0);
+    }
+  } else {
+    lcd.clear();
+    lcd.setCursor(0, 0);
+    lcd.print("Aguardando");
+    lcd.setCursor(0, 1);
+    lcd.print("Confirmacao");
+  }
 }
 
-void drawFixedBox() {
-  int sideLength = MAX_HEIGHT;
-
-  tft.drawRect(12, 50, sideLength, sideLength, ST77XX_WHITE);
+void incrasePos(int p, Servo s) {
+  for (pos = 0; pos < p; pos++) {
+    s.write(pos);
+    delay(15);
+  }
 }
 
-void drawWaterLevel(float level) {
-  tft.fillRect(12, 50 + MAX_HEIGHT - level, MAX_HEIGHT, level, ST77XX_BLUE); 
+void decrasePos(int p, Servo s, int x) {
+  for (pos = p; pos >= x; pos--) {
+    s.write(pos);
+    delay(15);
+  }
 }
 
-float calculateVolume(long height) {
-  float volume = (1000 - (3.14159 * RADIUS * RADIUS * height));
-  return volume;
+void esperarComDisplay(unsigned long segundos) {
+  for (unsigned long i = segundos; i > 0; i--) {
+    int dias = i / 86400;
+    int horas = (i % 86400) / 3600;
+    int minutos = (i % 3600) / 60;
+    int segs = i % 60;
+
+    lcd.setCursor(0, 0);
+    lcd.print("Aguardando...   ");
+
+    lcd.setCursor(0, 1);
+    lcd.print("T:");
+    lcd.print(dias); lcd.print("d ");
+    if (horas < 10) lcd.print("0");
+    lcd.print(horas); lcd.print("h ");
+    if (minutos < 10) lcd.print("0");
+    lcd.print(minutos); lcd.print("m ");
+    if (segs < 10) lcd.print("0");
+    lcd.print(segs); lcd.print("s ");
+
+    delay(1000);
+  }
+
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print("Abrindo...");
 }
 
-float calculateWaterLevel(float volume) {
-  float waterLevel = (100 * volume) / MAX_VOLUME;
-  return waterLevel;
-}
+void waitForButtonPress() {
+  while (digitalRead(7) == HIGH) {
+    delay(10);
+  }
 
-void drawText(char *text, uint16_t color, int size, int x, int y) {
-  tft.setCursor(x, y);
-  tft.setTextColor(color);
-  tft.setTextWrap(true);
-  tft.setTextSize(size);
-  tft.print(text);
+  while (digitalRead(7) == LOW) {
+    delay(10);
+  }
 }
